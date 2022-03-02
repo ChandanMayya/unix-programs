773450cd30e901e48af95cf1fa0c02c8407e1f38
ChandanMayya <chandanmogral@gmail.com>
2022-03-02 20:13:50 +0530

    2a.sh

--- 2a.sh
+++ 2a.sh
@@ -7,25 +7,25 @@ file2=$2
 if [ -e $file1 ]
 then
 	set -- `ls -ld $file1`
-	file1perm=$1
+	fp1=$1
 else
-	echo "File doesnt exit"
+	echo "$file1 doesnt exit"
 	exit
 fi
 if [ -e $file2 ]
 	then
 	set -- `ls -ld $file2`
-	file2perm=$1
+	fp2=$1
 else
-	echo "File doesnt exit"
+	echo "$file2 doesnt exit"
 exit
 fi
-if [ $file1perm = $file2perm ]
+if [ $fp1 = $fp2 ]
 then
-	echo "File Permission are identical"
-	echo "Permission is $file1perm"
+	echo "File Permission are identical!"
+	echo "Permissions are $fp1"
 else
 	echo "File Permission are not identical"
-	echo "$file1 Permission is $file1perm"
-	echo "$file2 Permission is $file2perm"
+	echo "$file1 Permission is $fp1"
+	echo "$file2 Permission is $fp2"
 fi
