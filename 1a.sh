263f6a2ae7b4b0f5692c1145aa96feefb5a8e261
ChandanMayya <chandanmogral@gmail.com>
2022-03-27 18:40:18 +0530

    Added 1a.sh

--- 1a.sh
+++ 1a.sh
@@ -3,12 +3,12 @@ then
 	dirnam=$1
 	if [ -d $dirnm ]
 	then
-		set -- `ls -Rl $dirnam |grep "^-" | tr -s " " | cut -d " " -f 5 | sort -n | tail -l`
+		set -- `ls -Rl $dirnam |grep "^-" | tr -s " " | cut -d " " -f 5,9| sort -n | tail -l`
 		echo "File name: $2"
 		echo "File size: $1"
 	else
 		echo "File: $dirnam not found!"
 	fi
 else
-	echo "Enter valid parameter"
+	echo "Enter a valid parameter"
 fi
